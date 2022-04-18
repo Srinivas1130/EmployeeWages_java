e1179db37535880e15601303822c10c5925a38b3
Srinivas1130 <srinivasachar1998@gmail.com>
2022-04-18 19:06:15 +0530

    Welcome to play Snake and Ladder

---
+++ snakeAndLadderUC1.java
@@ -0,0 +1,12 @@
+
+public class snakeAndLadderUC1 
+{
+	static final int START_POSITION = 0;
+
+    public static void main(String[] args) {
+        // welcome to snake ladder program.
+        System.out.println("Welcome to snake ladder game");
+        System.out.println("Player will start from zero position.");
+    }
+
+}
